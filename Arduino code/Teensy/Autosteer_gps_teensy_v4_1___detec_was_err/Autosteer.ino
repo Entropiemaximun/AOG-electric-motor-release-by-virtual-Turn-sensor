ca23631d555ca255af4a3b802c586f277d58dd1a
Entropiemaximun <88970536+Entropiemaximun@users.noreply.github.com>
2024-03-18 12:45:15 +0100

    Update Autosteer.ino
    update for small angle

--- Arduino code/Teensy/Autosteer_gps_teensy_v4_1___detec_was_err/Autosteer.ino
+++ Arduino code/Teensy/Autosteer_gps_teensy_v4_1___detec_was_err/Autosteer.ino
@@ -355,8 +355,8 @@ void autosteerLoop()
     }
     if (steerConfig.ShaftEncoder/* && pulseCount >= steerConfig.PulseCountMax*/)
     {
-      if ( abs(steerAngleError) >=  steerAngleOld ) {
-        if ( abs(steerAngleError) >  steerAngleOld ) {
+      if ( abs(steerAngleError) >  steerAngleOld and abs(steerAngleError) > 1 ) {  //modif
+        
           pulseCount = pulseCount + 5;
           if ( pulseCount >= steerConfig.PulseCountMax)
           {
@@ -364,9 +364,7 @@ void autosteerLoop()
             currentState = 1;
             previous = 0;
           }
-        } else {
-          if ( pulseCount > 2  ) pulseCount = pulseCount - 3;
-        }
+        
       } else {
         if ( pulseCount < 10  ) {
           pulseCount =  0;
