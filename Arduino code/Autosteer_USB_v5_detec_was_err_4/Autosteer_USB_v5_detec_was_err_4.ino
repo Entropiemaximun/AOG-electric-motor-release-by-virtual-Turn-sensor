76c916722b7614af671cc375a83c7af0849a1ec4
Entropiemaximun <88970536+Entropiemaximun@users.noreply.github.com>
2024-10-28 23:34:35 +0100

    Update Autosteer_USB_v5_detec_was_err_4.ino

--- Arduino code/Autosteer_USB_v5_detec_was_err_4/Autosteer_USB_v5_detec_was_err_4.ino
+++ Arduino code/Autosteer_USB_v5_detec_was_err_4/Autosteer_USB_v5_detec_was_err_4.ino
@@ -6,6 +6,7 @@
   * 
   * 
     Cancel original TurnSensor  to make a virtual TurnSensor  based on Error  Bricbric 2024
+       update Cancel small angle, add delay,stop low speed ;  danfoss choice  for  proportional instead  step  10 2024 
   */
   
 ////////////////// User Settings /////////////////////////  
